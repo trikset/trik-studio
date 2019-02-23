cc44e083b83be78a0af802450d485acebb97586b
iakov <jake.kirilenko@gmail.com>
2019-02-23 11:11:05 +0300

    Fix Python related libs installation paths

--- installer/packages/trik-studio/ru.qreal.root.trik.core/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.core/meta/prebuild-linux-gnu.sh
@@ -11,7 +11,7 @@ cp -pr $BIN_DIR/librobots-trik-qts-generator-library.so*                        
 cp -pr $BIN_DIR/librobots-trik-generator-base.so*                                  $PWD/../data/lib/
 cp -pr $BIN_DIR/librobots-trik-kit.so*                                             $PWD/../data/lib/
 cp -pr $BIN_DIR/librobots-trik-kit-interpreter-common.so*                          $PWD/../data/lib/
-cp -pr $BIN_DIR/libPythonQt-Qt*-Python*.so*                                        $PWD/../data/lib/
+cp -pr $BIN_DIR/libPythonQt{,_QtAll}-Qt*-Python*.so*                                        $PWD/../data/lib/
 cp -pr $BIN_DIR/libtrik*.so*                                                         $PWD/../data/lib/
 
 cp     $BIN_DIR/system.{py,js}                                                		 $PWD/../data/bin/
