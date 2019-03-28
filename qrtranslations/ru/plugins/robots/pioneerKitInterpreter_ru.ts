a785a1427a9329089c2aaeac57cc4916a0f52895
anastasiia-kornilova <kornilova.anastasiia@gmail.com>
2019-03-28 10:45:43 +0300

    Fixes for qrtranslations

--- qrtranslations/ru/plugins/robots/pioneerKitInterpreter_ru.ts
+++ qrtranslations/ru/plugins/robots/pioneerKitInterpreter_ru.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../../plugins/robots/interpreters/pioneerKitInterpreter/src/pioneerKitInterpreterPlugin.cpp" line="33"/>
         <source>Pioneer Kit</source>
-        <translation>Квадрокоптер &laquo;Пионер&raquo;</translation>
+        <translation>Квадрокоптер &#171;Пионер&#187;</translation>
     </message>
 </context>
 <context>
