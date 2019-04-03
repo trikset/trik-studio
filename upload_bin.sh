1991364e093989e83e56efb64b18e02a7583bfe0
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2019-04-03 20:05:15 +0300

    windeployqt all .exe files

--- upload_bin.sh
+++ upload_bin.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+#$APPVEYOR_BUILD_FOLDER\installer\build-trik-studio.sh $QTDIR\bin $QTIFWDIR\bin $BUILD_DIR
+
 pacman -Su --noconfirm sshpass zip
 cd $BUILD_DIR/
 ZIP=tsbin-$APPVEYOR_ACCOUNT_NAME-qt$QT.zip
 zip -r $ZIP bin
-ls
 sshpass -p "$password" ssh -q -o StrictHostKeyChecking=no "$server"
 sshpass -p "$password" scp $ZIP $server:/home/devops/dl/ts/fresh/
