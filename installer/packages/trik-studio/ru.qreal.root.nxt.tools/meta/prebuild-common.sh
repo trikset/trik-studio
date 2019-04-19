640c2cded3e6442d6f545170cccaf2d7a7758e25
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2019-04-20 01:44:31 +0300

    update nxt-tools for all platforms

--- installer/packages/trik-studio/ru.qreal.root.nxt.tools/meta/prebuild-common.sh
+++ installer/packages/trik-studio/ru.qreal.root.nxt.tools/meta/prebuild-common.sh
@@ -7,3 +7,8 @@ source $INSTALLER_ROOT/utils/common_utils.sh
 cd "$(dirname "$0")"
 rm -rf ../data
 mkdir -p $PWD/../data/
+
+if [ ! -d $INSTALLER_ROOT/nxt-tools ]
+then
+  git submodule update --init --recursive $INSTALLER_ROOT/nxt-tools
+fi
--- installer/packages/trik-studio/ru.qreal.root.nxt.tools/meta/prebuild-win32.sh
+++ installer/packages/trik-studio/ru.qreal.root.nxt.tools/meta/prebuild-win32.sh
@@ -7,7 +7,6 @@ source $INSTALLER_ROOT/utils/common_utils.sh
 
 cd $PWD/../data
 
-git submodule update --init --recursive
 cp -rf $INSTALLER_ROOT/nxt-tools/win/nxt-tools .
 dos2unix nxt-tools/compile.sh
 rm -rf bin
