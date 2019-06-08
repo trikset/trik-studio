29538de6b54bb4c5ca9f6a7daf36112332372576
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2019-06-08 17:09:00 +0300

    Fix after review

--- buildScripts/travis/after_script.sh
+++ buildScripts/travis/after_script.sh
@@ -5,7 +5,7 @@ case $TRAVIS_OS_NAME in
   osx)
     ;;
   linux)
-    if [ "$INSTALLER" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_REPO_SLUG" == "trikset/trikstudio" ] && [ "$TIMEOUT" != "true" ]; then 
+    if $INSTALLER && ! $TIMEOUT && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_REPO_SLUG" == "trikset/trik-studio" ] ; then
       $EXECUTOR bash -ic "echo Start build installer \
       && installer/build-trik-studio.sh /Qt/5.12.3/gcc_64/bin /Qt/Tools/QtInstallerFramework/3.0/bin . \
       && mv installer/trik-studio*installer* installer/trik-studio-installer-linux-$TRAVIS_BRANCH.run \
