37141f68fb1617ad20c6214180a342e374c50b28
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-06-18 18:49:59 +0300

    Add version logging, add explanation to port error

--- qrkernel/platformInfo.cpp
+++ qrkernel/platformInfo.cpp
@@ -23,59 +23,9 @@
 
 using namespace qReal;
 
-#if defined Q_OS_WIN
-QString prettyWindowsVersion(QSysInfo::WinVersion version)
-{
-	// Adapted from https://qt.gitorious.org/qt/thiago-intels-qtbase/commit/5757c458157bcb11da40c43e98eeb7a539d20912
-	switch (version)
-	{
-	case QSysInfo::WV_95:
-		return "95";
-	case QSysInfo::WV_98:
-		return "98";
-	case QSysInfo::WV_Me:
-		return "ME";
-	case QSysInfo::WV_NT:
-		return "NT";
-	case QSysInfo::WV_2000:
-		return "2000";
-	case QSysInfo::WV_XP:
-		return "XP";
-	case QSysInfo::WV_2003:
-		return "2003";
-	case QSysInfo::WV_VISTA:
-		return "Vista";
-	case QSysInfo::WV_WINDOWS7:
-		return "7";
-	case QSysInfo::WV_WINDOWS8:
-		return "8";
-	case QSysInfo::WV_WINDOWS8_1:
-		return "8.1";
-	case QSysInfo::WV_WINDOWS10:
-		return "10";
-	case QSysInfo::WV_CE:
-		return "CE";
-	case QSysInfo::WV_CENET:
-		return "CENET";
-	case QSysInfo::WV_CE_5:
-		return "CE5";
-	case QSysInfo::WV_CE_6:
-		return "CE6";
-	default:
-		return "";
-	}
-}
-#endif
-
 QString PlatformInfo::prettyOsVersion()
 {
-#if defined Q_OS_WIN32
-	return QString("Windows ") + prettyWindowsVersion(QSysInfo().windowsVersion());
-#elif defined Q_OS_LINUX
-	return "Linux";
-#elif defined Q_OS_MACOS
-	return QString("Mac ") + QSysInfo().macVersion();
-#endif
+	return QSysInfo::prettyProductName();
 }
 
 QString PlatformInfo::applicationDirPath()
@@ -91,7 +41,7 @@ QString PlatformInfo::applicationDirPath()
 		result.cdUp();					      // ../bin/debug/qreal-d.app/
 		result.cdUp();					      // ../bin/debug/
 	}
-	
+
 	return result.absolutePath();
 #else
 	return QCoreApplication::applicationDirPath();
