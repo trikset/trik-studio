9f70c9ecf333e4efab1ae83107e11621bba26698
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-06-19 10:35:18 +0300

    TravisCI: attempt to workaround brew bundle problem

--- buildScripts/travis/before_install.sh
+++ buildScripts/travis/before_install.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-#    brew update
+      brew bundle install && brew bundle check --verbose || brew update && brew bundle install && brew bundle check --verbose
     ;;
   linux)
     docker pull trikset/linux-builder
