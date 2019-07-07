d5871e1a3639564b6aee6f0f1b558b46c4a651fa
Iakov Kirilenko <iakov@trikset.com>
2019-07-07 09:42:06 +0300

    Travis: fix tagged build early termination

--- buildScripts/travis/has_only_skipped_files.sh
+++ buildScripts/travis/has_only_skipped_files.sh
@@ -3,6 +3,6 @@
 set -xueo pipefail
 IGNORED_FILES='*.md .cirrus.yml appveyor.yml buildScripts/docker/*'
 COMMIT_FILES=$(git diff --name-only ${TRAVIS_COMMIT_RANGE})
-for f in $COMMIT_FILES ; do
+for f in ${COMMIT_FILES:-"_UNDEFINED_FOR_TAGS_"} ; do
   ! ( for pattern in $IGNORED_FILES ; do [[ $f != $pattern ]] || exit 1 ; done ) || exit 1
 done
