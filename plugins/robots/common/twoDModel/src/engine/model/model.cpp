32398c8aa10068199d4ae893672d68958288a922
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2019-08-19 20:53:47 +0300

    Fixes for signal connection

--- plugins/robots/common/twoDModel/src/engine/model/model.cpp
+++ plugins/robots/common/twoDModel/src/engine/model/model.cpp
@@ -56,13 +56,15 @@ void Model::init(qReal::ErrorReporterInterface &errorReporter
 		errorReporter.addInformation(tr("The task is accomplished!"));
 		// Stopping cannot be performed immediately because we still have constraints to check in event loop
 		// and they need scene to be alive (in checker stopping interpretation means deleting all).
-		QTimer::singleShot(0, &interpreterControl, &kitBase::InterpreterControlInterface::stopAllInterpretation);
+		QTimer::singleShot(0, &interpreterControl,
+				[&interpreterControl](){ interpreterControl.stopAllInterpretation(); });
 	});
 	connect(mChecker.data(), &constraints::ConstraintsChecker::fail, this, [&](const QString &message) {
 		errorReporter.addError(message);
 		// Stopping cannot be performed immediately because we still have constraints to check in event loop
 		// and they need scene to be alive (in checker stopping interpretation means deleting all).
-		QTimer::singleShot(0, &interpreterControl, &kitBase::InterpreterControlInterface::stopAllInterpretation);
+		QTimer::singleShot(0, &interpreterControl,
+				[&interpreterControl](){ interpreterControl.stopAllInterpretation(); });
 	});
 	connect(mChecker.data(), &constraints::ConstraintsChecker::checkerError
 			, this, [&errorReporter](const QString &message) {
