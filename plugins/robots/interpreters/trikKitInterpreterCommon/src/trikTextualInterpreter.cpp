955ded4ce7d4e24559058834603e46ab9e296ee2
iakov <jake.kirilenko@gmail.com>
2019-09-01 10:54:35 +0300

    Fix spaces

--- plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp
+++ plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,222 +30,222 @@
 Q_DECLARE_METATYPE(utils::AbstractTimer*)
 
 const QString jsOverrides = "script.random = brick.random;script.wait = brick.wait;script.time = brick.time;"
-        "script.readAll = brick.readAll;script.timer = brick.timer;"
-        "arrayPPinternal = function(arg) {"
-            "var res = '[';"
-            "for(var i = 0; i < arg.length; i++) {"
-                "var separator = i + 1 != arg.length ? ', ' : '';"
-                "if (arg[i] instanceof Array) {"
-                    "res += arrayPPinternal(arg[i]) + separator;"
-                "} else { res += arg[i].toString() + separator; }"
-            "}"
-            "res += ']';"
-            "return res;"
-        "};"
-        "print = function() "
-        "{"
-            "var res = '';"
-            "var argLength = arguments.length;"
-            "for(var i = 0; i < argLength; i++) {"
-                "if (arguments[i] instanceof Array) {res += arrayPPinternal(arguments[i]);"
-                "} else {res += arguments[i].toString();}"
-                "if (i != argLength - 1) { res += \",\";}"
-            "};"
-            "brick.log(res);"
-            "return res;"
-        "};"
-        "script.system = function() {print('system is disabled in the interpreter');};";
+	"script.readAll = brick.readAll;script.timer = brick.timer;"
+	"arrayPPinternal = function(arg) {"
+		"var res = '[';"
+		"for(var i = 0; i < arg.length; i++) {"
+		"var separator = i + 1 != arg.length ? ', ' : '';"
+		"if (arg[i] instanceof Array) {"
+			"res += arrayPPinternal(arg[i]) + separator;"
+		"} else { res += arg[i].toString() + separator; }"
+		"}"
+		"res += ']';"
+		"return res;"
+	"};"
+	"print = function() "
+	"{"
+		"var res = '';"
+		"var argLength = arguments.length;"
+		"for(var i = 0; i < argLength; i++) {"
+		"if (arguments[i] instanceof Array) {res += arrayPPinternal(arguments[i]);"
+		"} else {res += arguments[i].toString();}"
+		"if (i != argLength - 1) { res += \",\";}"
+		"};"
+		"brick.log(res);"
+		"return res;"
+	"};"
+	"script.system = function() {print('system is disabled in the interpreter');};";
 
 const QString pyOverrides ="\ndef print(args): brick.log(args);\n";
 
 trik::TrikTextualInterpreter::TrikTextualInterpreter(
-        const QSharedPointer<trik::robotModel::twoD::TrikTwoDRobotModel> &model)
-    : mRunning(false), mBrick(model), mScriptRunner(mBrick, nullptr), mErrorReporter(nullptr)
+	const QSharedPointer<trik::robotModel::twoD::TrikTwoDRobotModel> &model)
+	: mRunning(false), mBrick(model), mScriptRunner(mBrick, nullptr), mErrorReporter(nullptr)
 {
-    connect(&mBrick, &TrikBrick::error, this, &TrikTextualInterpreter::reportError);
-    connect(&mBrick, &TrikBrick::warning, this, &TrikTextualInterpreter::reportWarning);
+	connect(&mBrick, &TrikBrick::error, this, &TrikTextualInterpreter::reportError);
+	connect(&mBrick, &TrikBrick::warning, this, &TrikTextualInterpreter::reportWarning);
 
-    auto atimerToScriptValue = [](QScriptEngine *engine, utils::AbstractTimer* const &in){
-        return engine->newQObject(in);
-    };
-    auto atimerFromScriptValue = [](const QScriptValue &object, utils::AbstractTimer* &out){
-        out = qobject_cast<utils::AbstractTimer*>(object.toQObject());
-    };
-    mScriptRunner.addCustomEngineInitStep([&atimerToScriptValue, &atimerFromScriptValue](QScriptEngine *engine){
-        qScriptRegisterMetaType<utils::AbstractTimer*>(engine, atimerToScriptValue, atimerFromScriptValue);
-    });
-    connect(&mScriptRunner, &trikScriptRunner::TrikScriptRunner::completed
-            , this, &TrikTextualInterpreter::scriptFinished);
+	auto atimerToScriptValue = [](QScriptEngine *engine, utils::AbstractTimer* const &in){
+	return engine->newQObject(in);
+	};
+	auto atimerFromScriptValue = [](const QScriptValue &object, utils::AbstractTimer* &out){
+	out = qobject_cast<utils::AbstractTimer*>(object.toQObject());
+	};
+	mScriptRunner.addCustomEngineInitStep([&atimerToScriptValue, &atimerFromScriptValue](QScriptEngine *engine){
+	qScriptRegisterMetaType<utils::AbstractTimer*>(engine, atimerToScriptValue, atimerFromScriptValue);
+	});
+	connect(&mScriptRunner, &trikScriptRunner::TrikScriptRunner::completed
+		, this, &TrikTextualInterpreter::scriptFinished);
 
-    using qReal::text::Languages;
-    using trikScriptRunner::ScriptType;
-    Languages::registerLanguage(Languages::javaScript(mScriptRunner.knownMethodNamesFor(ScriptType::JAVASCRIPT)));
+	using qReal::text::Languages;
+	using trikScriptRunner::ScriptType;
+	Languages::registerLanguage(Languages::javaScript(mScriptRunner.knownMethodNamesFor(ScriptType::JAVASCRIPT)));
 
-    if (!QProcessEnvironment::systemEnvironment().value("TRIK_PYTHONPATH").isEmpty()) {
-        Languages::registerLanguage(Languages::python(mScriptRunner.knownMethodNamesFor(ScriptType::PYTHON)));
-    }
+	if (!QProcessEnvironment::systemEnvironment().value("TRIK_PYTHONPATH").isEmpty()) {
+	Languages::registerLanguage(Languages::python(mScriptRunner.knownMethodNamesFor(ScriptType::PYTHON)));
+	}
 }
 
 trik::TrikTextualInterpreter::~TrikTextualInterpreter()
 {
-    abort();
+	abort();
 }
 
 void trik::TrikTextualInterpreter::interpretCommand(const QString &script)
 {
-    mScriptRunner.runDirectCommand(script);
+	mScriptRunner.runDirectCommand(script);
 }
 
 void trik::TrikTextualInterpreter::interpretScript(const QString &script, const QString &languageExtension)
 {
-    mRunning = true;
-    mBrick.processSensors(true);
-    if (languageExtension.contains("js")) {
-        mScriptRunner.run(jsOverrides + script);
-    } else if (languageExtension.contains("py")) {
-        QString updatedScript = script;
-        int lastIndexOfImport = updatedScript.lastIndexOf(QRegularExpression("^import .*"
-                , QRegularExpression::MultilineOption));
-        int indexOf = updatedScript.indexOf(QRegularExpression("$", QRegularExpression::MultilineOption)
-                , lastIndexOfImport);
-        updatedScript.insert(indexOf + 1, pyOverrides);
-        mScriptRunner.run(updatedScript, "dummyFile.py");
-    } else {
-        reportError(tr("Unsupported script file type"));
-    }
+	mRunning = true;
+	mBrick.processSensors(true);
+	if (languageExtension.contains("js")) {
+	mScriptRunner.run(jsOverrides + script);
+	} else if (languageExtension.contains("py")) {
+	QString updatedScript = script;
+	int lastIndexOfImport = updatedScript.lastIndexOf(QRegularExpression("^import .*"
+		, QRegularExpression::MultilineOption));
+	int indexOf = updatedScript.indexOf(QRegularExpression("$", QRegularExpression::MultilineOption)
+		, lastIndexOfImport);
+	updatedScript.insert(indexOf + 1, pyOverrides);
+	mScriptRunner.run(updatedScript, "dummyFile.py");
+	} else {
+	reportError(tr("Unsupported script file type"));
+	}
 }
 
 void trik::TrikTextualInterpreter::interpretScriptExercise(const QString &script
-        , const QString &inputs, const QString &languageExtension)
+	, const QString &inputs, const QString &languageExtension)
 {
-    Q_UNUSED(languageExtension)
-    mRunning = true;
-    mBrick.processSensors(true);
-    mBrick.setCurrentInputs(inputs);
-    QString newScript = jsOverrides + "script.writeToFile = null;\n" + script;
-    mScriptRunner.run(newScript);
+	Q_UNUSED(languageExtension)
+	mRunning = true;
+	mBrick.processSensors(true);
+	mBrick.setCurrentInputs(inputs);
+	QString newScript = jsOverrides + "script.writeToFile = null;\n" + script;
+	mScriptRunner.run(newScript);
 }
 
 void trik::TrikTextualInterpreter::abort()
 {
-    //mScriptRunner.abort();
-    mBrick.stopWaiting();
-    Q_ASSERT(mScriptRunner.thread() == thread());
-    QMetaObject::invokeMethod(&mScriptRunner, "abort", Qt::QueuedConnection); // just a wild test
-    mRunning = false; // reset brick?
-    mBrick.processSensors(false);
+	//mScriptRunner.abort();
+	mBrick.stopWaiting();
+	Q_ASSERT(mScriptRunner.thread() == thread());
+	QMetaObject::invokeMethod(&mScriptRunner, "abort", Qt::QueuedConnection); // just a wild test
+	mRunning = false; // reset brick?
+	mBrick.processSensors(false);
 }
 
 void trik::TrikTextualInterpreter::init()
 {
-    mBrick.init(); // very crucial. Maybe move into interpret methods?
+	mBrick.init(); // very crucial. Maybe move into interpret methods?
 }
 
 void trik::TrikTextualInterpreter::setErrorReporter(qReal::ErrorReporterInterface &errorReporter)
 {
-    mErrorReporter = &errorReporter;
+	mErrorReporter = &errorReporter;
 }
 
 bool trik::TrikTextualInterpreter::isRunning() const
 {
-    return mRunning;
+	return mRunning;
 }
 
 void trik::TrikTextualInterpreter::reportError(const QString &msg)
 {
-    mErrorReporter->addError(msg);
-    //	mBrick.abort(); what if there are more errors?
+	mErrorReporter->addError(msg);
+	//	mBrick.abort(); what if there are more errors?
 }
 
 void trik::TrikTextualInterpreter::reportWarning(const QString &msg)
 {
-    mErrorReporter->addWarning(msg);
+	mErrorReporter->addWarning(msg);
 }
 
 void trik::TrikTextualInterpreter::reportLog(const QString &msg)
 {
-    mErrorReporter->addInformation("log: " + msg);
+	mErrorReporter->addInformation("log: " + msg);
 }
 
 QString trik::TrikTextualInterpreter::initInputs(const QString &inputs) const
 {
-    auto jsValToStr = [this](const QJsonValue &val) -> QString {
-        switch (val.type()) {
-        case QJsonValue::Bool:
-            return val.toBool() ? "true" : "false";
-        case QJsonValue::Double:
-            return QString::number(val.Double);//maybe increase precision
-        case QJsonValue::String:
-            return QString("\"%1\"").arg(val.toString());
-        case QJsonValue::Array:
-            return QString(QJsonDocument(val.toArray()).toJson(QJsonDocument::Compact));
-        case QJsonValue::Object:
-            return QString(QJsonDocument(val.toObject()).toJson(QJsonDocument::Compact));
-        default:
-            mErrorReporter->addError(QObject::tr("Bogus input values"));
-            return "";
-        }
-    };
+	auto jsValToStr = [this](const QJsonValue &val) -> QString {
+	switch (val.type()) {
+	case QJsonValue::Bool:
+		return val.toBool() ? "true" : "false";
+	case QJsonValue::Double:
+		return QString::number(val.Double);//maybe increase precision
+	case QJsonValue::String:
+		return QString("\"%1\"").arg(val.toString());
+	case QJsonValue::Array:
+		return QString(QJsonDocument(val.toArray()).toJson(QJsonDocument::Compact));
+	case QJsonValue::Object:
+		return QString(QJsonDocument(val.toObject()).toJson(QJsonDocument::Compact));
+	default:
+		mErrorReporter->addError(QObject::tr("Bogus input values"));
+		return "";
+	}
+	};
 
-    QString result;
-    if (!inputs.isEmpty()) {
-        QString val;
-        QFile file;
-        file.setFileName(inputs);
-        if (file.open(QIODevice::ReadOnly)) {
-            val = file.readAll();
-            file.close();
-            QJsonDocument document = QJsonDocument::fromJson(val.toUtf8());
-            QJsonObject object = document.object();
-            for (const QString &name : object.keys()) {
-                QJsonValue value = object[name];
-                QString valueStr = jsValToStr(value);
-                QString line("var " + name + " = " + valueStr + ";\n");
-                result.append(line);
-            }
-        } else {
-            mErrorReporter->addError(QObject::tr("Error: File %1 couldn't be opened!").arg(inputs));
-        }
-    }
-    return result;
+	QString result;
+	if (!inputs.isEmpty()) {
+	QString val;
+	QFile file;
+	file.setFileName(inputs);
+	if (file.open(QIODevice::ReadOnly)) {
+		val = file.readAll();
+		file.close();
+		QJsonDocument document = QJsonDocument::fromJson(val.toUtf8());
+		QJsonObject object = document.object();
+		for (const QString &name : object.keys()) {
+		QJsonValue value = object[name];
+		QString valueStr = jsValToStr(value);
+		QString line("var " + name + " = " + valueStr + ";\n");
+		result.append(line);
+		}
+	} else {
+		mErrorReporter->addError(QObject::tr("Error: File %1 couldn't be opened!").arg(inputs));
+	}
+	}
+	return result;
 }
 
 void trik::TrikTextualInterpreter::setRunning(bool running)
 {
-    mRunning = running;
-    mBrick.processSensors(running);
+	mRunning = running;
+	mBrick.processSensors(running);
 }
 
 void trik::TrikTextualInterpreter::setCurrentDir(const QString &dir)
 {
-    mBrick.setCurrentDir(dir);
+	mBrick.setCurrentDir(dir);
 }
 
 QStringList trik::TrikTextualInterpreter::supportedRobotModelNames() const
 {
-    return {"TwoDRobotModelForTrikV62RealRobotModel", "TwoDRobotModelForTrikV6RealRobotModel"};
+	return {"TwoDRobotModelForTrikV62RealRobotModel", "TwoDRobotModelForTrikV6RealRobotModel"};
 }
 
 QStringList trik::TrikTextualInterpreter::knownMethodNames() const
 {
-    return mScriptRunner.knownMethodNames();
+	return mScriptRunner.knownMethodNames();
 }
 
 void trik::TrikTextualInterpreter::reinitRobotsParts()
 {
-    mBrick.reinitImitationCamera();
+	mBrick.reinitImitationCamera();
 }
 
 void trik::TrikTextualInterpreter::scriptFinished(const QString &error, int scriptId)
 {
-    Q_UNUSED(scriptId);
-    if (!error.isEmpty()) {
-        reportError(error);
-    }
+	Q_UNUSED(scriptId);
+	if (!error.isEmpty()) {
+	reportError(error);
+	}
 
-    if (mRunning) { /// @todo: figure out better place for this check - it should avoid double aborts
-        mRunning = false;
-        mBrick.processSensors(false);
-        emit completed();
-    }
+	if (mRunning) { /// @todo: figure out better place for this check - it should avoid double aborts
+	mRunning = false;
+	mBrick.processSensors(false);
+	emit completed();
+	}
 }
