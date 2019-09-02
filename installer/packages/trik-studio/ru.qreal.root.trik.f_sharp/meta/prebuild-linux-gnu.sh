f71509b47c38f4d9ca6ed0d3b45abec85b14bc41
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2019-09-02 13:39:57 +0300

    Add missing libraries

--- installer/packages/trik-studio/ru.qreal.root.trik.f_sharp/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.f_sharp/meta/prebuild-linux-gnu.sh
@@ -5,7 +5,7 @@ set -o errexit
 cd "$(dirname "$0")"
 
 mkdir -p $PWD/../data/lib/
-cp     $BIN_DIR/librobots-trik-f-sharp-generator-library.so                            $PWD/../data/lib/
+cp     $BIN_DIR/librobots-trik-f-sharp-generator-library.so*                            $PWD/../data/lib/
 
 # Cleaning up prebuild-common.sh results...
 mkdir -p $PWD/../data/resources/
--- installer/packages/trik-studio/ru.qreal.root.trik.pascal/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.pascal/meta/prebuild-linux-gnu.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 mkdir -p $PWD/../data/lib/
 mkdir -p $PWD/../data/lib/plugins/tools/kitPlugins/
 
-cp    $BIN_DIR/librobots-trik-pascal-abc-generator-library.so                              $PWD/../data/lib/
+cp    $BIN_DIR/librobots-trik-pascal-abc-generator-library.so*                              $PWD/../data/lib/
 cp    $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-pascal-abc-generator.so            $PWD/../data/lib/plugins/tools/kitPlugins/
 
 # Cleaning up prebuild-common.sh results...
