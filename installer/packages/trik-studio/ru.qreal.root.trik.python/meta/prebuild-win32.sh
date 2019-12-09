73427cccd7e9c87cffcd9fbc778b2e5d61bc0c15
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2019-12-09 13:24:55 +0300

    Get python version from python3.dll

--- installer/packages/trik-studio/ru.qreal.root.trik.python/meta/prebuild-win32.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.python/meta/prebuild-win32.sh
@@ -7,7 +7,9 @@ cd "$(dirname "$0")"
 cp    $BIN_DIR/robots-trik-python-generator-library.dll                              $PWD/../data/
 cp    $BIN_DIR/plugins/tools/kitPlugins/robots-trik-v62-python-generator.dll         $PWD/../data/plugins/tools/kitPlugins/
 
-python_ver=3.7.5 # TMP
+unix_path=$BIN_DIR/python3.dll
+win_path="$(cygpath -w $unix_path)"
+python_ver=$(powershell -command "(Get-Item $win_path).VersionInfo.ProductVersion")
 cache_dir="$(cygpath $APPDATA)"/$PRODUCT/installer_cache
 if [ -z $(file -b $BIN_DIR/trik-studio | grep -Eo "^PE32 ") ] ; then PY_BIT=amd64 ; else PY_BIT=win32 ; fi
 mkdir -p "$cache_dir"
@@ -16,6 +18,7 @@ cd "$cache_dir"
 if [ ! -d PYTHON_$python_ver ]
 then
 # download python
+echo Download python ver $python_ver
 python_zip="https://www.python.org/ftp/python/$python_ver/python-$python_ver-embed-$PY_BIT.zip"
 
 curl -L -s -o python.zip $python_zip
