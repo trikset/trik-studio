c2c4cc9fa9773f27f5a3d035346df7a1346e8b9c
Andrei <andrei.khodko@gmail.com>
2019-12-09 21:04:19 +0300

    Cadacy issue fix

--- buildScripts/travis/vera_translation.sh
+++ buildScripts/travis/vera_translation.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -xuevo pipefail
 
-CODECOV=true
 $EXECUTOR buildScripts/travis/runVera++.sh
-git_diff=$( { git diff --diff-filter=d --name-only ${TRAVIS_COMMIT_RANGE} || true ; } \
+git_diff=$( { git diff --diff-filter=d --name-only "${TRAVIS_COMMIT_RANGE}" || true ; } \
   | xargs -r file -i | sed -e "s|\(.*\):.*text/x-c.*|\1|g" -e "/:/d")
   [[ -z "${git_diff}" ]] || $EXECUTOR vera++ --error --root buildScripts/vera++ --profile strict <<< "$git_diff"
 
