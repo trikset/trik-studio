0988e3e79bb1e0b5e2f245ec5b6283dfb5f0526e
Andrei <andrei.khodko@gmail.com>
2019-12-09 20:07:06 +0300

    Pre-build stage added

---
+++ buildScripts/travis/vera_translation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xuevo pipefail
+  
+git_diff=$( { git diff --diff-filter=d --name-only ${TRAVIS_COMMIT_RANGE} || true ; } \
+  | xargs -r file -i | sed -e "s|\(.*\):.*text/x-c.*|\1|g" -e "/:/d")
+  [[ -z "${git_diff}" ]] || $EXECUTOR vera++ --error --root buildScripts/vera++ --profile strict <<< "$git_diff"
+
+$EXECUTOR lupdate studio.pro plugins/robots/editor/*/translations.pro && $EXECUTOR buildScripts/travis/checkStatus.sh
