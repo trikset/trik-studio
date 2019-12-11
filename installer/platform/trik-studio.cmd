3d953bfa9d5ea6f83a5b5736c75a346d9543a9f7
Andrei <andrei.khodko@gmail.com>
2019-12-11 19:19:09 +0300

    Set common path

---
+++ installer/platform/trik-studio.cmd
@@ -0,0 +1,7 @@
+set ARCH=32
+set PYTHON_PATH=C:\Python37
+set TRIK_PYTHONPATH=%PYTHON_PATH%\Lib
+set PYTHON_LIB=%PYTHON_PATH%\libs
+set QTDIR=C:\Qt\5.12\mingw73_%ARCH%
+set MINGW=%QTDIR%\..\..\Tools\mingw730_%ARCH%
+set PATH=%PYTHON_PATH%;%QTDIR%\bin;%MINGW%\bin;C:\msys64\mingw%ARCH%\bin;C:\msys64\usr\bin;%SystemRoot%;%SystemRoot%\system32;
