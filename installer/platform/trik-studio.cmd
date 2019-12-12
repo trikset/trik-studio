f9427e9f6a295b6e732e915254e0f1504e5de7c9
Andrei <andrei.khodko@gmail.com>
2019-12-12 18:28:39 +0300

    Split path to make it shorter

--- installer/platform/trik-studio.cmd
+++ installer/platform/trik-studio.cmd
@@ -4,4 +4,5 @@ set TRIK_PYTHONPATH=%PYTHON_PATH%\Lib
 set PYTHON_LIB=%PYTHON_PATH%\libs
 set QTDIR=C:\Qt\5.12\mingw73_%ARCH%
 set MINGW=%QTDIR%\..\..\Tools\mingw730_%ARCH%
-set PATH=%PYTHON_PATH%;%QTDIR%\bin;%MINGW%\bin;C:\msys64\mingw%ARCH%\bin;C:\msys64\usr\bin;%SystemRoot%;%SystemRoot%\system32;
+set PATH=C:\msys64\usr\bin;%SystemRoot%;%SystemRoot%\system32;
+set PATH=%PYTHON_PATH%;%QTDIR%\bin;%MINGW%\bin;C:\msys64\mingw%ARCH%\bin;%PATH%
