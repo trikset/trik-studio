b2b4eeffca3217b6b8477e9bd8ec067d3eb5f4d4
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2020-01-22 02:37:09 +0300

    Lupdate

--- qrtranslations/fr/qrutils_fr.ts
+++ qrtranslations/fr/qrutils_fr.ts
@@ -88,7 +88,7 @@
         <translation>Division entière par zéro</translation>
     </message>
     <message>
-        <location filename="../../qrutils/graphicsUtils/abstractItem.cpp" line="+572"/>
+        <location filename="../../qrutils/graphicsUtils/abstractItem.cpp" line="+573"/>
         <source>Remove</source>
         <translation>Supprimer</translation>
     </message>
