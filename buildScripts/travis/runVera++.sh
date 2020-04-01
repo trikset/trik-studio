37d627c87baeddca1795aef779e061f24491e322
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-04-01 14:26:37 +0300

    Improve Vera++ check on Travis

--- buildScripts/travis/runVera++.sh
+++ buildScripts/travis/runVera++.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+set -xueo pipefail
 tclsh buildScripts/vera++/generatePaths.tcl
 vera++ --error -p allRules --root buildScripts/vera++ <buildScripts/vera++/params
+git_diff=$( { git diff --diff-filter=d --name-only ${TRAVIS_COMMIT_RANGE:-upstream/master HEAD} || true ; } \
+  | xargs -r file -i | sed -e "s|\(.*\):.*text/x-c.*|\1|g" -e "/:/d")
+  [[ -z "${git_diff}" ]] || vera++ --error --root buildScripts/vera++ --profile strict <<< "$git_diff"
+
