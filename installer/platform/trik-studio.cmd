6a19846ba5a1a54e3caae49882a68bb1e08767d0
iakov <iakov@users.noreply.github.com>
2020-04-12 15:57:25 +0300

    Set empty name to windows

--- installer/platform/trik-studio.cmd
+++ installer/platform/trik-studio.cmd
@@ -2,4 +2,4 @@
 title TRIK Studio Safe Mode
 FOR /F "tokens=* USEBACKQ" %%F IN (`where java 2^>NUL`) DO (SET JAVA_PATH=%%~dpF)
 set PATH=%SystemRoot%;%SystemRoot%\system32;%JAVA_PATH%
-start %~dp0trik-studio.exe %*
+start "" %~dp0trik-studio.exe %*
