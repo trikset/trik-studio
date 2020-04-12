12768da6e539bc51b87f907605a94ba59eb33d43
iakov <iakov@users.noreply.github.com>
2020-04-12 15:51:55 +0300

    Hot fix for .cmd

--- installer/platform/trik-studio.cmd
+++ installer/platform/trik-studio.cmd
@@ -1,5 +1,5 @@
 @echo off
 title TRIK Studio Safe Mode
-FOR /F "tokens=* USEBACKQ" %%F IN (`where java 2>NUL`) DO (SET JAVA_PATH=%%~dpF)
-set PATH=%SystemRoot%;%SystemRoot%\system32;%JAVA_PATH%;
-%~dp0trik-studio.exe %*
+FOR /F "tokens=* USEBACKQ" %%F IN (`where java 2^>NUL`) DO (SET JAVA_PATH=%%~dpF)
+set PATH=%SystemRoot%;%SystemRoot%\system32;%JAVA_PATH%
+start %~dp0trik-studio.exe %*
