e366905a7ac6d7b25c208ff17a4ba8ada88f805c
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-04-23 19:24:52 +0300

    macOS installer fixes

--- installer/packages/trik-studio/ru.qreal.root.trik.core/meta/prebuild-mac.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.core/meta/prebuild-mac.sh
@@ -7,10 +7,8 @@ source $INSTALLER_ROOT/utils/mac_utils.sh
 
 mkdir -p "$BUNDLE_CONTENTS/Lib/plugins/editors"
 cp     $BIN_DIR/plugins/editors/libtrikMetamodel.dylib                                "$BUNDLE_CONTENTS/Lib/plugins/editors/"
-cp -pR $BIN_DIR/librobots-trik-generator-base*.dylib                                  "$BUNDLE_CONTENTS/Lib"
 cp -pR $BIN_DIR/librobots-trik-kit*.dylib                                             "$BUNDLE_CONTENTS/Lib"
 cp -pR $BIN_DIR/librobots-trik-kit-interpreter-common*.dylib                          "$BUNDLE_CONTENTS/Lib"
-cp -pR $BIN_DIR/libtrik*.dylib                                            	          "$BUNDLE_CONTENTS/Lib"
 
 
 fix_qreal_dependencies "$BUNDLE_CONTENTS/Lib/plugins/editors/libtrikMetamodel.dylib"
