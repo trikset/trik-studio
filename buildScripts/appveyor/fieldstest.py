06ac1b75b0250c9ef7208191cdd169b93777f39c
ikonovalova <irina7l3@mail.ru>
2020-07-03 10:04:39 +0300

    Add 2D-model qrs tests

---
+++ buildScripts/appveyor/fieldstest.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import os
+from typing import Iterable
+
+
+def get_files(dir_path: str) -> Iterable[str]:
+	return sorted(os.path.join(d, file) for d, ns, f in os.walk(os.path.expanduser(dir_path)) for file in f)
+	
+if __name__=='__main__':
+	bin_path = sys.argv[1]
+	num_failed_tests = 0
+	for f in list(get_files(sys.argv[2])):
+		output = subprocess.run([bin_path, '-b', '-s', '5', f], capture_output=True)
+		print("Test %s. " % f + "Return code : %d. " % output.returncode + "Message: %s" % output.stderr)
+		if (output.returncode != 0):
+			print("Solution failed with return code %d " % output.returncode)
+			num_failed_tests += 1
+	if num_failed_tests == 0:
+		sys.exit(0)
+	else:
+		sys.exit(1)
