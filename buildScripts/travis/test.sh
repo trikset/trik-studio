2506deed9b8e4592a0c378609fb57cf4ab8a6994
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 09:41:31 +0300

    Clean up Codacy warnings

--- buildScripts/travis/test.sh
+++ buildScripts/travis/test.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 curl --output tests_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 $EXECUTOR bash -lixc "7z x tests_qrs.7z"
-if [ $TRAVIS_OS_NAME = "linux" ]
+if [ "$TRAVIS_OS_NAME" = "linux" ]
 then
-	$EXECUTOR bin/$CONFIG/build-checker-installer.sh
+	$EXECUTOR "bin/$CONFIG/build-checker-installer.sh"
 fi
 $EXECUTOR bash -lixc "export ASAN_OPTIONS=detect_leaks=0:detect_stack_use_after_return=1:fast_unwind_on_malloc=0:use_sigaltstack=0 LSAN_OPTIONS=suppressions=lsan.supp:fast_unwind_on_malloc=0 \
 && python3.\${TRIK_PYTHON3_VERSION_MINOR} ./buildScripts/tests/fieldstest.py bin/$CONFIG/2D-model testing"
