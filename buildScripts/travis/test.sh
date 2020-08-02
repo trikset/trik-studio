a20102c114f7933c7907a97cbdd35c3cf5df3f08
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 12:26:35 +0300

    Minimize affection of time consuming sanitizers options

--- buildScripts/travis/test.sh
+++ buildScripts/travis/test.sh
@@ -6,5 +6,5 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 fi
 $EXECUTOR bash -lixc "curl --output tests_qrs.7z https://dl.trikset.com/edu/.solutions20200701/test.7z \
 	&& 7z x tests_qrs.7z \
-	&& export ASAN_OPTIONS=detect_leaks=0:detect_stack_use_after_return=1:fast_unwind_on_malloc=0:use_sigaltstack=0 LSAN_OPTIONS=suppressions=lsan.supp:fast_unwind_on_malloc=0 \
+	&& export ASAN_OPTIONS=detect_leaks=0 \
 	&& python3.\${TRIK_PYTHON3_VERSION_MINOR} ./buildScripts/tests/fieldstest.py bin/$CONFIG/2D-model testing"
