495cc515eebed837ab20d0a65044a1a6a49ab617
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 18:11:17 +0300

    Specify platform explicitly

--- buildScripts/travis/test.sh
+++ buildScripts/travis/test.sh
@@ -7,4 +7,4 @@ fi
 $EXECUTOR bash -lixc "curl --output tests_qrs.7z https://dl.trikset.com/edu/.solutions20200701/test.7z \
 	&& 7z x tests_qrs.7z \
 	&& export ASAN_OPTIONS=detect_leaks=0 \
-	&& python3.\${TRIK_PYTHON3_VERSION_MINOR} ./buildScripts/tests/fieldstest.py bin/$CONFIG/2D-model testing"
+	&& python3.\${TRIK_PYTHON3_VERSION_MINOR} ./buildScripts/tests/fieldstest.py bin/$CONFIG/2D-model minimal testing"
