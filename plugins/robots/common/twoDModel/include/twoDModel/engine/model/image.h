ccc4da63b5166dbadbfc571647a75670bd4a34e7
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-08-03 15:53:14 +0300

    Smooth rendering for .svg sensor images

--- plugins/robots/common/twoDModel/include/twoDModel/engine/model/image.h
+++ plugins/robots/common/twoDModel/include/twoDModel/engine/model/image.h
@@ -62,7 +62,7 @@ public:
 	void setPath(const QString &path);
 
 	/// Draws image with \a painter inside the \a rect considering \a zoom.
-	void draw(QPainter &painter, const QRect &rect, qreal zoom = 1.0);
+	void draw(QPainter &painter, const QRect &rect, qreal zoom = 1.0) const;
 
 	/// Returns imageId for this image.
 	QString imageId() const;
