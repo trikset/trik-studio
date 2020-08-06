08990a1fcc62c85c5c0ae3e1838f75a999768cdf
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-06 23:35:31 +0500

    Fixes after review
    Co-authored-by: iakov <iakov@users.noreply.github.com>

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -7,4 +7,4 @@ curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z
 curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
 chmod +x installer.run
 ./installer.run --platform minimal --script ./installer/trik_studio_installscript.qs
-python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" minimal testing
+env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
