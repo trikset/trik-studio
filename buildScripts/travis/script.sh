92cd03a0f0acd3facd4fc037c5a2ba102db59480
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-08-06 07:59:14 +0300

    TravisCI: use default LSAN_OPTIONS
    Linux: docker requires additional capabilities:
    https://github.com/google/sanitizers/issues/764
    
    macOS: detect_leaks=1 is unsupported as of Jul 2020

--- buildScripts/travis/script.sh
+++ buildScripts/travis/script.sh
@@ -40,7 +40,7 @@ $EXECUTOR bash -lixc "\
 && sh -c 'make -j2 qmake_all 1>>build.log 2>&1' \
 && sh -c 'make -j2 all 1>>build.log 2>&1' \
 && sh -c \"cd bin/$CONFIG && ls\" \
-&& sh -c \"export ASAN_OPTIONS=detect_leaks=1:detect_stack_use_after_return=1:fast_unwind_on_malloc=0:use_sigaltstack=0 LSAN_OPTIONS=suppressions=lsan.supp:fast_unwind_on_malloc=0:verbosity=0:log_threads=0  DISPLAY=:0 && cd bin/$CONFIG && $TESTS\""
+&& sh -xc \"export ASAN_OPTIONS=$(if [[ $TRAVIS_OS_NAME == linux ]]; then echo 'detect_leaks=1:'; else echo -n ''; fi)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:use_sigaltstack=0 DISPLAY=:0 && cd bin/$CONFIG && $TESTS\""
 df -h .
 $EXECUTOR bash -ic buildScripts/travis/checkStatus.sh
 
