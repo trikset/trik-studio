cb5dad4f8f18fb49aa1eaaa071c04d46e247cacc
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-24 19:35:00 +0500

    Apply suggestions from code review
    Co-authored-by: iakov <iakov@users.noreply.github.com>

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -4,16 +4,16 @@ set -euxo pipefail
 curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 7z x test_qrs.7z
 
-case $TRAVIS_OS_NAME in
-  linux)
+case $(uname -s) in
+  Linux)
     curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
     chmod +x installer.run
     ./installer.run --platform minimal --script ./installer/trik_studio_installscript.qs
     env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
     ;;
-  osx)
-    ./installer/install_studio_mac.sh ./installer/
-    env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/TRIK Studio.app/Contents/MacOS/2D-model" testing
+  Darwin)
+    env TRIK_STUDIO_INSTALL_DIR="$HOME/T S" ./installer/install_studio_mac.sh
+    env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/T S/TRIK Studio.app/Contents/MacOS/2D-model" testing
     ;;
   *) exit 1 ;;
 esac
