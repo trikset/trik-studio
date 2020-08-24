7d3e1de934b51e831759c18efda1aaf066cbd65f
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-24 20:21:13 +0300

    Avoid path with spaces

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -12,8 +12,8 @@ case $(uname -s) in
     env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
     ;;
   Darwin)
-    env TRIK_STUDIO_INSTALL_DIR="$HOME/T S" ./installer/install_studio_mac.sh
-    env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/T S/TRIK Studio.app/Contents/MacOS/2D-model" testing
+    env TRIK_STUDIO_INSTALL_DIR="$HOME/TS" ./installer/install_studio_mac.sh
+    env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TS/TRIK Studio.app/Contents/MacOS/2D-model" testing
     ;;
   *) exit 1 ;;
 esac
