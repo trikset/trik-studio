8440d5924a095b344b1907836a74458634781c7c
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-09-13 18:21:14 +0300

    Disable integration tests for Junior

--- buildScripts/travis/test.sh
+++ buildScripts/travis/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-$EXECUTOR bash -lic "curl --output tests_qrs.7z https://dl.trikset.com/edu/.solutions20200701/testing_small.7z \
+true || $EXECUTOR bash -lic "curl --output tests_qrs.7z https://dl.trikset.com/edu/.solutions20200701/testing_small.7z \
 	&& 7z x tests_qrs.7z \
 	&& env ASAN_OPTIONS=detect_leaks=1:use_sigaltstack=0 \
 	       LSAN_OPTIONS=suppressions=$PWD/bin/$CONFIG/lsan.supp:print_suppressions=0 \
