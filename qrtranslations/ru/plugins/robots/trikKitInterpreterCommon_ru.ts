321fce7247a0686c9df4cc4efbecaed4727c2c11
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2020-10-24 10:10:43 +0300

    Add pyOverrides after import in exercise too

--- qrtranslations/ru/plugins/robots/trikKitInterpreterCommon_ru.ts
+++ qrtranslations/ru/plugins/robots/trikKitInterpreterCommon_ru.ts
@@ -4,7 +4,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="+202"/>
+        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="+206"/>
         <source>Bogus input values</source>
         <translation>Неподходящие значения аргументов</translation>
     </message>
@@ -284,8 +284,8 @@
 <context>
     <name>trik::TrikTextualInterpreter</name>
     <message>
-        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="-99"/>
-        <location line="+16"/>
+        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="-98"/>
+        <location line="+15"/>
         <source>Unsupported script file type</source>
         <translation>Неверный формат файла</translation>
     </message>
