fba81466b9c68318291774cbe19a7c41d9b4328e
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2020-11-12 08:29:31 +0300

    Lupdate

--- qrtranslations/fr/plugins/robots/twoDModel_fr.ts
+++ qrtranslations/fr/plugins/robots/twoDModel_fr.ts
@@ -313,7 +313,7 @@
 <context>
     <name>twoDModel::items::ImageItem</name>
     <message>
-        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/items/imageItem.cpp" line="+75"/>
+        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/items/imageItem.cpp" line="+78"/>
         <source>Image (I)</source>
         <translation type="unfinished"></translation>
     </message>
--- qrtranslations/fr/qrutils_fr.ts
+++ qrtranslations/fr/qrutils_fr.ts
@@ -88,7 +88,7 @@
         <translation>Division entière par zéro</translation>
     </message>
     <message>
-        <location filename="../../qrutils/graphicsUtils/abstractItem.cpp" line="+570"/>
+        <location filename="../../qrutils/graphicsUtils/abstractItem.cpp" line="+574"/>
         <source>Remove</source>
         <translation>Supprimer</translation>
     </message>
