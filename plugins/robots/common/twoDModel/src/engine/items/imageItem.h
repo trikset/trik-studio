28912c6446a1e0037dd6db6f0afcd3f79a104b54
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2020-11-12 08:24:35 +0300

    Add aligment for images

--- plugins/robots/common/twoDModel/src/engine/items/imageItem.h
+++ plugins/robots/common/twoDModel/src/engine/items/imageItem.h
@@ -47,6 +47,7 @@ public:
 	void drawItem(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
 	void drawExtractionForItem(QPainter* painter) override;
 	QPainterPath resizeArea() const override;
+	void resizeItem(QGraphicsSceneMouseEvent *event) override;
 
 	QDomElement serialize(QDomElement &parent) const override;
 	void deserialize(const QDomElement &element) override;
@@ -94,6 +95,7 @@ private:
 	QSharedPointer<model::Image> mImage;
 
 	bool mBackgroundRole {};
+	QPointF mEstimatedPos;
 };
 
 }
