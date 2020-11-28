93856a24b51dbe0d7f9e4249d39925922204896e
Iakov Kirilenko <iakov@trikset.com>
2020-11-28 17:57:10 +0300

    Allow to run test script locally
    Use like `env CONFIG=release TRIK_PYTHON3_VERSION_MINOR=9 ../studio/buildScripts/travis/test.sh` from build folder

--- buildScripts/travis/vera_translation.sh
+++ buildScripts/travis/vera_translation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -xuevo pipefail
 TRIK_LINTER_OK=true
-$EXECUTOR buildScripts/travis/runVera++.sh || TRIK_LINTER_OK=false
-$EXECUTOR bash -lic 'lupdate studio.pro plugins/robots/editor/*/translations.pro' && $EXECUTOR buildScripts/travis/checkStatus.sh || TRIK_LINTER_OK=false
-
+if which vera++ >/dev/null 2>&1 ; then buildScripts/travis/runVera++.sh || TRIK_LINTER_OK=false ; fi
+LUPDATE=lupdate
+which lupdate-pro >/dev/null 2>&1 && LUPDATE=lupdate-pro
+$LUPDATE studio.pro plugins/robots/editor/*/translations.pro || TRIK_LINTER_OK=false
+buildScripts/travis/checkStatus.sh || TRIK_LINTER_OK=false
 "$TRIK_LINTER_OK"
