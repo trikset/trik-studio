f9fbbeccfb490e14ed968d028742c78e16794dcd
Andrei Khodko <andrei.khodko@gmail.com>
2020-12-26 18:11:32 +0300

    Azure fix (#1218)
    * Fix env variable name and no cache installing
    * Correct branch name
    * Use commit tag or branch name
    * Fix var
    * Delete duplicate code

--- buildScripts/azure/build_installer.sh
+++ buildScripts/azure/build_installer.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -euxo pipefail
+BRANCH_NAME="${BRANCH_NAME:-$BUILD_SOURCEBRANCHNAME}"
 QTBIN=${QTBIN:-$($EXECUTOR  bash -c "make qmake -n | sed 's#/qmake.*\$##g'")}
 case $AGENT_OS in
   Darwin)
     QTIFWBIN=$HOME/qtifw/bin
-    TSNAME=trik-studio-installer-mac-$BUILD_SOURCEBRANCH.dmg
+    TSNAME=trik-studio-installer-mac-$BRANCH_NAME.dmg
     export TRIK_PYTHON3_VERSION_MINOR="$(python3 -V | sed 's#^Python [0-9]\.\([0-9]\)\.[0-9]*$#\1#g')"
     ;;
   Linux)
     QTIFWBIN=/opt/qtifw/bin
     #QTIFWBIN=$($EXECUTOR bash -c  'find /Qt/Tools/QtInstallerFramework/ -maxdepth 2 -name bin -type d -print0 | sort -Vrz | head -zn 1')
-    TSNAME=trik-studio-installer-linux-$BUILD_SOURCEBRANCH.run
+    TSNAME=trik-studio-installer-linux-$BRANCH_NAME.run
     ;;
   *) exit 1 ;;
 esac
@@ -26,7 +27,7 @@ then
       $EXECUTOR bash -ic "\
       echo Start build checker archive \
       && bin/$CONFIG/build-checker-installer.sh \
-      && sshpass -p $password rsync -avze 'ssh -o StrictHostKeyChecking=no' bin/$CONFIG/trik_checker.tar.xz $server:dl/ts/fresh/checker/checker-${AGENT_OS}-$CONFIG-$BUILD_SOURCEBRANCH.tar.xz \
+      && sshpass -p $password rsync -avze 'ssh -o StrictHostKeyChecking=no' bin/$CONFIG/trik_checker.tar.xz $server:dl/ts/fresh/checker/checker-${AGENT_OS}-$CONFIG-$BRANCH_NAME.tar.xz \
       || false \
 "
       fi
