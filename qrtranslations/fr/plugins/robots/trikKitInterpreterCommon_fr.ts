87edb587d0751a9314d4a326931316f601c06e0d
iakov <iakov@users.noreply.github.com>
2021-12-13 21:28:44 +0300

    Fix crash when mailbox is not enabled (#1486)

--- qrtranslations/fr/plugins/robots/trikKitInterpreterCommon_fr.ts
+++ qrtranslations/fr/plugins/robots/trikKitInterpreterCommon_fr.ts
@@ -4,7 +4,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="+169"/>
+        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="+171"/>
         <source>Bogus input values</source>
         <translation type="unfinished"></translation>
     </message>
