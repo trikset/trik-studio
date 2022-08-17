7795e1266802cf13ddb3bf068d00335f69fc0864
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2022-08-17 21:48:40 +0300

    Add correct TRIK_PYTHONPATH for tests

--- buildScripts/azure/build.sh
+++ buildScripts/azure/build.sh
@@ -51,7 +51,9 @@ $EXECUTOR bash -lic " set -xueo pipefail; \
      export ASAN_OPTIONS=$(if [[ $AGENT_OS == Linux ]]; then echo 'detect_leaks=1:'; else echo -n ''; fi)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:use_sigaltstack=0 ; \
      export LSAN_OPTIONS=suppressions=$PWD/bin/$CONFIG/lsan.supp:print_suppressions=0 ; \
      export DISPLAY=:0 ; \
-     env QT_QPA_PLATFORM=minimal make check -k -s && ( set +eux ; cd bin/$CONFIG && $TESTS ) ; \
+     export TRIK_PYTHONPATH=\${TRIK_PYTHONPATH:-\$(python3.\${TRIK_PYTHON3_VERSION_MINOR} -c 'import sys; import os; print(os.pathsep.join(sys.path))')} ;\
+     env ; \
+     make check -k -s && ( set +eux ; cd bin/$CONFIG && $TESTS ) ; \
    }\
 "
 
