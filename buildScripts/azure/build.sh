7fd5bf3e5d70ca476199b2873507f6e0932b3128
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2022-08-29 19:57:35 +0300

    Enable Asan for integration tests

--- buildScripts/azure/build.sh
+++ buildScripts/azure/build.sh
@@ -52,6 +52,7 @@ $EXECUTOR bash -lic " set -xueo pipefail; \
      export LSAN_OPTIONS=suppressions=$PWD/bin/$CONFIG/lsan.supp:print_suppressions=0 ; \
      export DISPLAY=:0 ; \
      export TRIK_PYTHONPATH=\${TRIK_PYTHONPATH:-\$(python3.\${TRIK_PYTHON3_VERSION_MINOR} -c 'import sys; import os; print(os.pathsep.join(sys.path))')} ;\
+     export PYTHONMALLOC=malloc ; \
      env ; \
      make check -k -s && ( set +eux ; cd bin/$CONFIG && $TESTS ) ; \
    }\
