c16f5d51b474b22ad9962222baa7bd8a13d1a56d
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2022-09-12 00:21:40 +0300

    Set global git option to ignore directory owner

--- buildScripts/azure/install_Linux.sh
+++ buildScripts/azure/install_Linux.sh
@@ -2,3 +2,4 @@
     set -euxo pipefail
     docker pull trikset/linux-builder
     docker run --cap-add SYS_PTRACE -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
+    docker exec builder git config --global --add safe.directory '*'
