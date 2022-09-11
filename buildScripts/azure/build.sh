68de7b365003454a362d47b194ff39f8ec403574
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2022-09-12 00:21:40 +0300

    Set global git option to ignore directory owner

--- buildScripts/azure/build.sh
+++ buildScripts/azure/build.sh
@@ -41,7 +41,6 @@ $EXECUTOR bash -lic " set -xueo pipefail; \
 && eval 'export PKG_CONFIG_PATH=\`python3.\${TRIK_PYTHON3_VERSION_MINOR}-config --prefix\`/lib/pkgconfig' \
 && rm -f .qmake.cache \
 &&  { echo -n 'Remove broken object files ' ; find . -name '*.o' -type f -print0 | xargs -0 file --mime-type | grep -v 'application/x-object' | cut -f 1 -d ':' | xargs -t rm -f _RM_REQUIRES_ARGUMENTS_BUT_BSD_XARGS_IS_MISSING_R_PARAMETER_ARGUMENT || : ; echo Done ; } \
-&& git config --add safe.directory "*" \
 && qmake -Wall PYTHON3_VERSION_MINOR=\$TRIK_PYTHON3_VERSION_MINOR CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro \
 && make -j2 qmake_all 2>&1 | tee -a build.log \
 && ccache -s \
