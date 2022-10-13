dea9341f5dc34e56babb977a774cf902fe328672
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2022-10-13 14:44:47 +0300

    Azure: split build scripts

--- buildScripts/azure/build.sh
+++ buildScripts/azure/build.sh
@@ -27,35 +27,14 @@ sloppiness=time_macros,pch_defines,include_file_ctime,include_file_mtime,file_st
 max_size=1200M
 EOF
 
-$EXECUTOR bash -lic " set -xueo pipefail; \
-   export CCACHE_CONFIGPATH=$CCACHE_CONFIGPATH LANG=C \
-&& ccache -V && ccache -p \
-&& which g++ \
-&& g++ --version \
-&& which qmake \
-&& qmake -query \
-&& ccache -sz \
-&& { which python3 && python3 -V || true ; } \
-&& { which python && python -V || true ; } \
-&& eval 'export PKG_CONFIG_PATH=\`python3.\${TRIK_PYTHON3_VERSION_MINOR}-config --prefix\`/lib/pkgconfig' \
-&& rm -f .qmake.cache \
-&&  { echo -n 'Remove broken object files ' ; find . -name '*.o' -type f -print0 | xargs -0 file --mime-type | grep -v 'application/x-object' | cut -f 1 -d ':' | xargs -t rm -f _RM_REQUIRES_ARGUMENTS_BUT_BSD_XARGS_IS_MISSING_R_PARAMETER_ARGUMENT || : ; echo Done ; } \
-&& qmake -Wall PYTHON3_VERSION_MINOR=\$TRIK_PYTHON3_VERSION_MINOR CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro \
-&& make -j2 qmake_all 2>&1 | tee -a build.log \
-&& ccache -s \
-&& make -j2 all 2>&1 | tee -a build.log \
-&& ccache -s \
-&& ls bin/$CONFIG \
-&& { export QT_QPA_PLATFORM=minimal ; \
-     export ASAN_OPTIONS=$(if [[ $AGENT_OS == Linux ]]; then echo 'detect_leaks=1:'; else echo -n ''; fi)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:use_sigaltstack=0 ; \
-     export LSAN_OPTIONS=suppressions=$PWD/bin/$CONFIG/lsan.supp:print_suppressions=0 ; \
-     export DISPLAY=:0 ; \
-     export TRIK_PYTHONPATH=\${TRIK_PYTHONPATH:-\$(python3.\${TRIK_PYTHON3_VERSION_MINOR} -c 'import sys; import os; print(os.pathsep.join(sys.path))')} ;\
-     export PYTHONMALLOC=malloc ; \
-     env ; \
-     make check -k -s && ( set +eux ; cd bin/$CONFIG && $TESTS ) ; \
-   }\
-"
+$EXECUTOR env \
+CCACHE_CONFIGPATH="$CCACHE_CONFIGPATH" \
+CONFIG="$CONFIG" \
+QMAKE_EXTRA="$QMAKE_EXTRA" \
+PROJECT="$PROJECT" \
+AGENT_OS="$AGENT_OS" \
+buildScripts/azure/build_internal.sh
+
 
 df -h .
 $EXECUTOR bash -ic buildScripts/azure/checkStatus.sh
