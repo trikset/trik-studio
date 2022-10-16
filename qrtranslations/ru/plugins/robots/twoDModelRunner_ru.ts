0b02e78ca298c3d7820eca778dab98473b3c513f
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2022-10-16 19:56:17 +0300

    Lupdate

--- qrtranslations/ru/plugins/robots/twoDModelRunner_ru.ts
+++ qrtranslations/ru/plugins/robots/twoDModelRunner_ru.ts
@@ -69,7 +69,7 @@ In background mode the session will be terminated just after the execution ended
 <context>
     <name>twoDModel::Runner</name>
     <message>
-        <location filename="../../../../plugins/robots/checker/twoDModelRunner/runner.cpp" line="+201"/>
+        <location filename="../../../../plugins/robots/checker/twoDModelRunner/runner.cpp" line="+202"/>
         <source>Robot console</source>
         <translation>Консоль робота</translation>
     </message>
