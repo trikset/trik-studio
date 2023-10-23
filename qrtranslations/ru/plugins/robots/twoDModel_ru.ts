496465718ee65c9631df069fcca5fc05f7863ea7
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2023-10-23 02:06:43 +0200

    Lupdate

--- qrtranslations/ru/plugins/robots/twoDModel_ru.ts
+++ qrtranslations/ru/plugins/robots/twoDModel_ru.ts
@@ -573,7 +573,7 @@
 <context>
     <name>twoDModel::engine::TwoDModelEngineFacade</name>
     <message>
-        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/twoDModelEngineFacade.cpp" line="+189"/>
+        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/twoDModelEngineFacade.cpp" line="+190"/>
         <source>Realistic physics&apos; must be turned on to enjoy skittles and balls</source>
         <translation>Реалистичная физика должна быть включена для взаимодействия с кеглями и мячами</translation>
     </message>
@@ -1071,7 +1071,7 @@
 <context>
     <name>twoDModel::view::TwoDModelWidget</name>
     <message>
-        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/view/twoDModelWidget.cpp" line="+318"/>
+        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/view/twoDModelWidget.cpp" line="+323"/>
         <source>Warning</source>
         <translation>Предупреждение</translation>
     </message>
