d066762e32fc2d88f4584fa14f2afc7c2045fa7d
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2024-03-17 18:55:58 +0300

    Use QtIFw 4.6.1

--- buildScripts/azure/install_qtifw_mac.sh
+++ buildScripts/azure/install_qtifw_mac.sh
@@ -3,9 +3,9 @@ TRIK_QT_INSTALL_DIR=${TRIK_QT_INSTALL_DIR:-$HOME/TRIK_QtIfw}
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 set -ueo pipefail
 #The latest with Qt 5.12 as minimal required, 4.3.0 requires Qt 5.15
-QTIFW_VERSION=4.2.0
+QTIFW_VERSION=4.6.1
 D=/tmp/qtifw-installer.dmg
-test -r "$D" || curl -L -o "$D" "http://download.qt-project.org/official_releases/qt-installer-framework/$QTIFW_VERSION/QtInstallerFramework-macOS-x64-4.2.0.dmg"
+test -r "$D" || curl -L -o "$D" "http://download.qt-project.org/official_releases/qt-installer-framework/$QTIFW_VERSION/QtInstallerFramework-macOS-x64-${QTIFW_VERSION}.dmg"
 P=$(hdiutil attach "$D" -noverify -noautofsck | grep -o '/Volumes/.*$')
 I=$(find "$P" -type f -perm +1 -path '*Contents/MacOS/*' -print | head -n 1 )
 env TRIK_QT_INSTALL_DIR="$TRIK_QT_INSTALL_DIR" "$I" --verbose --no-force-installations --show-virtual-components --script "$SCRIPT_DIR/../../plugins/robots/thirdparty/trikRuntime/trikRuntime/docker/qt_scriptinstall.qs"
