cb30093696c89a3881a31ddd15da28ca46411c81
MinyazevR <eeuriset8@gmail.com>
2024-08-14 12:32:37 +0300

    Fix format error

--- buildScripts/github/run_tests.sh
+++ buildScripts/github/run_tests.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export QT_QPA_PLATFORM=minimal
 export ASAN_OPTIONS=$(if [[ $RUNNER_OS == Linux ]]; then echo 'detect_leaks=1:'; else echo -n ''; fi)detect_stack_use_after_return=1:fast_unwind_on_malloc=0
 export LSAN_OPTIONS=suppressions=$PWD/bin/lsan.supp:print_suppressions=0
