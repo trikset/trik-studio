5de57eebe207ad4ba57822d8931c1707deafe224
iakov <iakov@users.noreply.github.com>
2024-08-31 14:34:37 +0300

    Latest TRIK runtime with huge improvements (#1768)
    * Support Qt5.15
    * Support Python versions up to 3.12
    * Fix few memory leaks in Python scripting engine
    * Fix memory corruption in Python engine
    * New improved tests with PYTHONDEVMODE=1

--- buildScripts/github/build_internal.sh
+++ buildScripts/github/build_internal.sh
@@ -10,9 +10,10 @@ qmake -query
 ccache -sz
 { which python3 && python3 -V || true ; }
 { which python && python -V || true ; }
-export PKG_CONFIG_PATH=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix)/lib/pkgconfig
+export PYTHON_DIR=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix)
+export PKG_CONFIG_PATH="$PYTHON_DIR/lib/pkgconfig"
 rm -f .qmake.cache
-qmake -Wall PYTHON3_VERSION_MINOR=$TRIK_PYTHON3_VERSION_MINOR CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro
+qmake -Wall PYTHON_VERSION=3.$TRIK_PYTHON3_VERSION_MINOR PYTHON_PATH=/usr CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro
 make -j $(nproc) qmake_all 2>&1 | tee -a build.log
 ccache -s
 make -j $(nproc) all 2>&1 | tee -a build.log
