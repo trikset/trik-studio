19559483934a941480f35a566f3d9db204b3cc0f
MinyazevR <eeuriset8@gmail.com>
2024-10-20 17:10:32 +0300

    Lupdate

--- qrtranslations/fr/plugins/robots/trikKitInterpreterCommon_fr.ts
+++ qrtranslations/fr/plugins/robots/trikKitInterpreterCommon_fr.ts
@@ -4,7 +4,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="+171"/>
+        <location filename="../../../../plugins/robots/interpreters/trikKitInterpreterCommon/src/trikTextualInterpreter.cpp" line="+164"/>
         <source>Bogus input values</source>
         <translation type="unfinished"></translation>
     </message>
@@ -238,7 +238,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+57"/>
+        <location line="+61"/>
         <source>TRIK_PYTHONPATH must be set correctly to run Python script.</source>
         <translation type="unfinished"></translation>
     </message>
