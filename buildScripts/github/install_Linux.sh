32b8b23187610811b9e727f24af41fa7d5ad6bba
MinyazevR <eeuriset8@gmail.com>
2024-10-24 15:47:50 +0300

    Add Ubuntu CI in github vm and rewrite rockylinux CI

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -2,13 +2,37 @@
 set -euxo pipefail
 
 ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+BUILD_INSTALLER=${BUILD_INSTALLER:-false}
+GCC_VERSION=${GCC_VERSION:-13}
+TRIK_PYTHON="python3.${TRIK_PYTHON3_VERSION_MINOR}"
 
 if [ "$ID" = "altlinux" ]; then
-    apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
-    libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
-    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
-else
-    docker pull trikset/linux-builder
-    docker run --cap-add SYS_PTRACE -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
-    docker exec builder git config --global --add safe.directory '*'
+  apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
+  libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
+  qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
+elif [ "$ID" = "ubuntu" ]; then
+  sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+  ccache curl libusb-1.0-0-dev make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
+  libudev-dev python3.${TRIK_PYTHON3_VERSION_MINOR}-dev qtbase5-private-dev
+elif [[ "$ID" = "rocky" || "$ID" = '"rocky"' ]]; then
+  yum update -y && yum install -y epel-release
+  yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
+  git-core ccache zlib-devel rsync python3-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time   
+  yum install -y libxkbcommon-x11 qt5-qtbase-gui #no desire to enumerate all required libraries for QtIFw
+  yum install -y pulseaudio-libs-glib2 # to run TS and 2D-model even with `minimal` platform
+
+  # yum install -y qt5-qtscript-devel qt5-qttools-devel qt5-qtmultimedia-devel qt5-qtserialport-devel qt5-qtsvg-devel qt5-qtbase-devel
+  "$TRIK_PYTHON" -m pip install -U pip
+  "$TRIK_PYTHON" -m pip install aqtinstall
+  aqt install-qt linux desktop "$TRIK_QT_VERSION" -O /Qt -m qtscript --archives qtbase qtmultimedia qtsvg qtscript qttools qtserialport qtimageformats icu qtwayland
+  QT_ROOT_DIR=$(ls -1d /Qt/"$TRIK_QT_VERSION"*/gcc_64 | head -n 1)
+
+  echo "$QT_ROOT_DIR/bin" >> $GITHUB_PATH
+  echo "source scl_source enable gcc-toolset-$GCC_VERSION" >> ~/.bash_profile
+fi
+
+if [ "$BUILD_INSTALLER" = "true" ]; then
+  "$TRIK_PYTHON" -m pip install -U pip
+  "$TRIK_PYTHON" -m pip install aqtinstall
+  "$TRIK_PYTHON" -m aqt install-tool -O /Qt linux desktop tools_ifw
 fi
