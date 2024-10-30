868066ee0c318c0d32b975acbe6f1578b1b24368
MinyazevR <eeuriset8@gmail.com>
2024-10-30 16:06:26 +0300

    Split the environment for the installer and the build environment for the distribution

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -2,9 +2,7 @@
 set -euxo pipefail
 
 ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
-BUILD_INSTALLER=${BUILD_INSTALLER:-false}
 GCC_VERSION=${GCC_VERSION:-13}
-TRIK_PYTHON="python3.${TRIK_PYTHON3_VERSION_MINOR}"
 
 if [ "$ID" = "altlinux" ]; then
   apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
@@ -17,16 +15,10 @@ elif [ "$ID" = "ubuntu" ]; then
 elif [[ "$ID" = "rocky" || "$ID" = '"rocky"' ]]; then
   yum update -y && yum install -y epel-release
   yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
-  git-core ccache zlib-devel rsync "$TRIK_PYTHON"-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time
+  git-core ccache zlib-devel rsync python3-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time
   yum install -y libxkbcommon-x11 qt5-qtbase-gui #no desire to enumerate all required libraries for QtIFw
   yum install -y pulseaudio-libs-glib2 # to run TS and 2D-model even with `minimal` platform
   yum install -y qt5-qtscript-devel qt5-qttools-devel qt5-qtmultimedia-devel qt5-qtserialport-devel qt5-qtsvg-devel qt5-qtbase-devel qt5-qtbase-private-devel \
   qt5-qtwayland-devel
   echo "source scl_source enable gcc-toolset-$GCC_VERSION" >> ~/.bash_profile
-fi
-
-if [ "$BUILD_INSTALLER" = "true" ]; then
-  "$TRIK_PYTHON" -m pip install -U pip
-  "$TRIK_PYTHON" -m pip install aqtinstall
-  "$TRIK_PYTHON" -m aqt install-tool -O $PWD/Qt linux desktop tools_ifw
 fi
---
+++ buildScripts/github/install_installer_environment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euxo pipefail
+
+GCC_VERSION=${GCC_VERSION:-13}
+
+TRIK_PYTHON="python3.${TRIK_PYTHON3_VERSION_MINOR}"
+TRIK_QT_VERSION=${TRIK_QT_VERSION:-5.15}
+
+yum update -y && yum install -y epel-release
+yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
+git-core ccache zlib-devel rsync "$TRIK_PYTHON"-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time
+yum install -y libxkbcommon-x11 qt5-qtbase-gui
+yum install -y pulseaudio-libs-glib2
+echo "source scl_source enable gcc-toolset-$GCC_VERSION" >> ~/.bash_profile
+
+"$TRIK_PYTHON" -m pip install -U pip
+"$TRIK_PYTHON" -m pip install aqtinstall
+"$TRIK_PYTHON" -m aqt install-tool -O $PWD/Qt linux desktop tools_ifw
