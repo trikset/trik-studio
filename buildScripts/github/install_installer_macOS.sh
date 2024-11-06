4f2c95de20e2106d8a56c1de26ad88a5371347a7
MinyazevR <eeuriset8@gmail.com>
2024-11-06 15:53:21 +0300

    Change application path

--- buildScripts/github/install_installer_macOS.sh
+++ buildScripts/github/install_installer_macOS.sh
@@ -6,11 +6,11 @@ sudo cp -rf "/Volumes/$INSTALLER_NAME/$INSTALLER_NAME.app" $GITHUB_WORKSPACE
 sudo hdiutil detach /Volumes/"$INSTALLER_NAME"
 ./$INSTALLER_NAME.app/Contents/MacOS/$INSTALLER_NAME --verbose --script trik_studio_installscript.qs
 
-BIN_DIR="/Applications/TRIKStudio/TRIK Studio.app/Contents" && echo "BIN_DIR=$BIN_DIR" >> $GITHUB_ENV
+BIN_DIR="/Applications/TRIKStudio/TRIK Studio.app/Contents/MacOS" && echo "BIN_DIR=$BIN_DIR" >> $GITHUB_ENV
 LIB_DIR="/Applications/TRIKStudio/TRIK Studio.app/Contents/Lib" && echo "LIB_DIR=$LIB_DIR" >> $GITHUB_ENV
-APP_DIR="/Applications/TRIKStudio/TRIK Studio.app/Contents" && echo "APP_DIR=$APP_DIR" >> $GITHUB_ENV
+APP_DIR="/Applications/TRIKStudio/TRIK Studio.app/Contents/MacOS" && echo "APP_DIR=$APP_DIR" >> $GITHUB_ENV
 
-ls "/Applications/TRIKStudio/TRIK Studio.app/Contents"
+ls "/Applications/TRIKStudio/TRIK Studio.app/Contents/MacOS"
 export DYLD_LIBRARY_PATH="$LIB_DIR"
 "$BIN_DIR"/2D-model --version
 "$BIN_DIR"/checkapp --version
@@ -19,5 +19,5 @@ export DYLD_LIBRARY_PATH="$LIB_DIR"
 "$APP_DIR"/trik-studio --version
 
 cd "$LIB_DIR"
-ls "*.dylib" | xargs otool -L | grep "not found" || exit 0
+ls *.dylib | xargs otool -L | grep "not found" || exit 0
 exit 1
