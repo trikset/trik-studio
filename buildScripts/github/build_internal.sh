bad59e8797a38b62e540296cd003da32985f0569
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-11-11 22:01:12 +0300

    Improve CI (rewrite RockylLinux CI, Ubuntu CI to GHA) (#1803)
    * Add Ubuntu CI in github vm and rewrite rockylinux CI
    * Enable cpu feature sse4.2
    * Split the environment for the installer and the build environment for the distribution
    * Add qtwayland dependecies
    * Delete unnecessary files
    * Add auto remove script for artifact for next pipeline and add option for installscript.qs
    * Add SDK Qt packages for installer environment
    * Add Qt installation to GITHUB_PATH
    * Add qtwaylandcompositor module to aqtinstall
    * Add worflow for install TRIK Studio and switch to macOS-13
    * Switch distro rockylinux build to python3.11
    * Add ldd search for non system lib
    * Install dependencies to install TRIK Studio on macOS
    * Add step to test interpreter on existing .qrs
    * Add test: Remove previous installation
    * Switch to offscreen platform
    * Switch from altlinux container to alt

--- buildScripts/github/build_internal.sh
+++ buildScripts/github/build_internal.sh
@@ -11,7 +11,6 @@ ccache -sz
 { which python3 && python3 -V || true ; }
 { which python && python -V || true ; }
 export PYTHON_DIR=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix)
-export PKG_CONFIG_PATH="$PYTHON_DIR/lib/pkgconfig"
 rm -f .qmake.cache
 qmake -Wall PYTHON_VERSION=3.$TRIK_PYTHON3_VERSION_MINOR PYTHON_PATH=/usr CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro
 make -j $(nproc) qmake_all 2>&1 | tee -a build.log
--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -2,13 +2,23 @@
 set -euxo pipefail
 
 ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+GCC_VERSION=${GCC_VERSION:-13}
 
 if [ "$ID" = "altlinux" ]; then
-    apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
-    libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
-    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
-else
-    docker pull trikset/linux-builder
-    docker run --cap-add SYS_PTRACE -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
-    docker exec builder git config --global --add safe.directory '*'
+  apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
+  libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
+  qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
+elif [ "$ID" = "ubuntu" ]; then
+  sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+  ccache curl libusb-1.0-0-dev make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
+  libudev-dev python3.${TRIK_PYTHON3_VERSION_MINOR}-dev qtbase5-private-dev qtwayland5
+elif [[ "$ID" = "rocky" || "$ID" = '"rocky"' ]]; then
+  yum update -y && yum install -y epel-release
+  yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
+  git-core ccache zlib-devel rsync python3.${TRIK_PYTHON3_VERSION_MINOR}-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time
+  yum install -y libxkbcommon-x11 qt5-qtbase-gui #no desire to enumerate all required libraries for QtIFw
+  yum install -y pulseaudio-libs-glib2 # to run TS and 2D-model even with `minimal` platform
+  yum install -y qt5-qtscript-devel qt5-qttools-devel qt5-qtmultimedia-devel qt5-qtserialport-devel qt5-qtsvg-devel qt5-qtbase-devel qt5-qtbase-private-devel \
+  qt5-qtwayland
+  echo "source scl_source enable gcc-toolset-$GCC_VERSION" >> ~/.bash_profile
 fi
