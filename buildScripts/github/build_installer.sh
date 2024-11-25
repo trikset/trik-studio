d82d33ebc4a88e57738d51a28709ca7e042d3cc7
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-11-25 18:30:24 +0300

    Replace "/" with "-" in installer name

--- buildScripts/github/build_installer.sh
+++ buildScripts/github/build_installer.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 BRANCH_NAME="${BRANCH_NAME:-${GITHUB_REF#refs/heads/}}"
 BRANCH_NAME="${BRANCH_NAME#refs/tags/}"
+BRANCH_NAME=${BRANCH_NAME//\//-}
 
 QTBIN=${QTBIN:-$($EXECUTOR  bash -c 'eval $(make qmake -n | cut -f 1 -d " ") -query QT_INSTALL_BINS')}
 case $RUNNER_OS in
