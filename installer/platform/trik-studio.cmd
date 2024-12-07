b3d4c0e0cefb247e280d82e078e394eb7292d202
iakov <iakov@users.noreply.github.com>
2024-12-07 18:53:45 +0300

    Win EV3:Take 1st available Java in PATH

--- installer/platform/trik-studio.cmd
+++ installer/platform/trik-studio.cmd
@@ -1,5 +1,5 @@
 @echo off
 title TRIK Studio Safe Mode
-FOR /F "tokens=* USEBACKQ" %%F IN (`where java 2^>NUL`) DO (SET JAVA_PATH=%%~dpF)
+FOR /f "usebackq tokens=1* delims=:" %%F IN (`where java 2^>NUL ^| findstr /n "^"`) DO @IF %%F == 1 ( ECHO(Using Java: "%%G" && SET JAVA_PATH=%%~dpG )
 set PATH=%SystemRoot%;%SystemRoot%\system32;%JAVA_PATH%
 %~dp0trik-studio.exe "%*"
