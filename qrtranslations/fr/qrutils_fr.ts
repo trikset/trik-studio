5df429be244bbfc323f27892d9e2a0cba8bfca9f
AlexandrKudrya <alexandr.kudrya@gmail.com>
2024-12-19 05:20:05 +0300

    fix: translations add

--- qrtranslations/fr/qrutils_fr.ts
+++ qrtranslations/fr/qrutils_fr.ts
@@ -88,7 +88,7 @@
         <translation>Division entière par zéro</translation>
     </message>
     <message>
-        <location filename="../../qrutils/graphicsUtils/abstractItem.cpp" line="+594"/>
+        <location filename="../../qrutils/graphicsUtils/abstractItem.cpp" line="+595"/>
         <source>Remove</source>
         <translation>Supprimer</translation>
     </message>
