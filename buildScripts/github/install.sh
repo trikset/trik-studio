ea51cd173a56617b36a2069c5dc948afe52b1e19
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-12-28 16:21:36 +0300

    Switch to QtIFW 3.2 for Linux (#1874)
    * Swith QtIFW to 3.2 for Linux and make common scripts for multiple platforms
    * Run tests on the installer even if the previous ones fall
    * Add output param for Windows build

---
+++ buildScripts/github/install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -euxo pipefail
+TRIK_PYTHON=python3.${TRIK_PYTHON3_VERSION_MINOR}
+
+install_qt(){
+  # Usage: install_qt <os> <platform-type> <qt-version> <path-to-install-qt> <modules> <archives>
+  # TODO: add <addittional-options>
+  "$TRIK_PYTHON" -m venv venv
+  . ./venv/bin/activate
+  "$TRIK_PYTHON" -m pip install -U pip
+  "$TRIK_PYTHON" -m pip install aqtinstall
+  if [ -z "${6+x}" ]; then
+    "$TRIK_PYTHON" -m aqt install-qt "$1" "$2" "$3" -O "$4" -m "${modules[@]}"
+  else
+    "$TRIK_PYTHON" -m aqt install-qt "$1" "$2" "$3" -O "$4" -m "${modules[@]}" --archives "${archives[@]}"
+  fi
+  if [ "$BUILD_INSTALLER" = "true" ]; then
+    [ -d $HOME/qtifw ] || env TRIK_QTIFW_INSTALL_DIR="$HOME/qtifw" "$(dirname $(realpath ${BASH_SOURCE[0]}))"/install_qtifw.sh
+  fi
+  deactivate
+}
+
+case "$(uname)" in
+  Darwin)
+    export HOMEBREW_NO_INSTALL_CLEANUP=1
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    brew tap "hudochenkov/sshpass"
+    TRIK_BREW_PACKAGES="ccache coreutils libusb pkg-config gnu-sed sshpass p7zip python@3.${TRIK_PYTHON3_VERSION_MINOR}"
+    for pkg in $TRIK_BREW_PACKAGES ; do
+      p="${pkg##*/}"
+      p="${p%.*}"
+      brew install --quiet "$pkg" || brew upgrade "$pkg" || brew link --force "$pkg" || echo "Failed to install/upgrade $pkg"
+    done
+    modules=("qtscript")
+    install_qt mac desktop "${TRIK_QT_VERSION}" "$HOME/Qt" $modules
+    sudo xcode-select -s /Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer
+    xcodebuild -showsdks
+    xcrun -sdk macosx --show-sdk-path
+    ;;
+  Linux)
+    ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+    
+    if [ "$ID" = "altlinux" ]; then
+      sudo apt-get update && sudo apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
+      libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
+      qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
+    elif [ "$ID" = "ubuntu" ]; then
+      sudo apt-get update && sudo apt-get install -y --no-install-recommends ccache curl libusb-1.0-0-dev \
+      make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
+      libudev-dev "$TRIK_PYTHON"-dev qtbase5-private-dev qtwayland5
+    elif [[ "$ID" = "rocky" || "$ID" = '"rocky"' ]]; then
+      GCC_VERSION=${GCC_VERSION:-13}
+      sudo yum update -y &&  sudo yum install -y --setopt=install_weak_deps=False epel-release
+      sudo yum install --allowerasing -y --setopt=install_weak_deps=False sudo which libusbx-devel curl \
+      wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} git-core ccache \
+      zlib-devel rsync "$TRIK_PYTHON"-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time findutils
+  
+      #no desire to enumerate all required libraries for QtIFw
+      # pulseaudio-libs-glib2 to run TS and 2D-model even with `minimal` platform
+      sudo yum install -y --setopt=install_weak_deps=False pulseaudio-libs-glib2 libxkbcommon-x11 qt5-qtbase-gui  libwayland-{server,client,cursor}
+
+      echo $INSTALL_INSTALLER_ENVIRONMENT
+      if [ "$INSTALL_INSTALLER_ENVIRONMENT" != "true" ]; then
+        sudo yum install -y --setopt=install_weak_deps=False qt5-qtscript-devel qt5-qttools-devel qt5-qtmultimedia-devel qt5-qtserialport-devel \
+        qt5-qtsvg-devel qt5-qtbase-devel qt5-qtbase-private-devel qt5-qtwayland
+      else
+        sudo yum install -y --setopt=install_weak_deps=False libX11-xcb libXext libxkbcommon-x11 fontconfig freetype libXrender
+        #libQt5WaylandCompositor.so.5.15: libQt5Quick.so.5 libQt5Qml.so.5 libQt5QmlModels.so.5 
+        modules=("qtscript" "qtwaylandcompositor")
+        archives=("qtbase" "qtmultimedia" "qtsvg" "qtscript" "qttools" "qtserialport" "qtimageformats" "icu" "qtwayland" "qtdeclarative")
+        install_qt linux desktop "$TRIK_QT_VERSION" "$HOME/Qt" $modules $archives
+        QT_ROOT_DIR=$(ls -1d "$HOME"/Qt/$TRIK_QT_VERSION*/gcc_64 | head -n 1)
+        echo "$QT_ROOT_DIR/bin" >> $GITHUB_PATH
+      fi
+      echo "source scl_source enable gcc-toolset-$GCC_VERSION" >> ~/.bash_profile
+    fi
+    ;;
+  *) exit 1 ;;
+esac
--- buildScripts/github/run_tests.sh
+++ buildScripts/github/run_tests.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-export QT_QPA_PLATFORM=minimal
-export ASAN_OPTIONS=$(if [[ $RUNNER_OS == Linux ]]; then echo 'detect_leaks=1:'; else echo -n ''; fi)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:detect_odr_violation=0 # dirty hack for libtrikQsLog and libtrikRuntimeQsLog
+set -euo pipefail
+
+export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-minimal}
+
+# dirty hack for libtrikQsLog and libtrikRuntimeQsLog
+export ASAN_OPTIONS=$(if [[ $RUNNER_OS == Linux ]]; then echo 'detect_leaks=1:'; else echo -n ''; fi)detect_stack_use_after_return=1:fast_unwind_on_malloc=0:detect_odr_violation=0
 export LSAN_OPTIONS=suppressions=$PWD/bin/lsan.supp:print_suppressions=0
 export DISPLAY=:0
 export TRIK_PYTHONPATH=${TRIK_PYTHONPATH:-$(python3.${TRIK_PYTHON3_VERSION_MINOR} -c 'import sys; import os; print(os.pathsep.join(sys.path))')}
-export PYTHONMALLOC=malloc
+export PYTHONMALLOC=${PYTHONMALLOC:-malloc}
+export PYTHONIOENCODING=utf-8
 env
+
+
 make check -k -s && ( set +eux ; cd "bin" && eval "$TESTS" )
+
+[ -r tests_qrs.7z ] || curl -Lo tests_qrs.7z https://dl.trikset.com/edu/.solutions20200701/testing_small.7z
+which 7z &> /dev/null && 7z -y x tests_qrs.7z || 7za x tests_qrs.7z
+python3.${TRIK_PYTHON3_VERSION_MINOR} "$(dirname $(realpath ${BASH_SOURCE[0]}))"/../tests/fieldstest.py bin/2D-model testing_small
