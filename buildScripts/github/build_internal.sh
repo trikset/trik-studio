da4adf6a95209f35c65b6e9e5c67204f68dbe15b
MinyazevR <eeuriset8@gmail.com>
2025-08-22 12:30:19 +0300

    Fix typo

--- buildScripts/github/build_internal.sh
+++ buildScripts/github/build_internal.sh
@@ -10,11 +10,10 @@ qmake -query
 ccache -sz
 { which python3 && python3 -V || true ; }
 { which python && python -V || true ; }
-mkdir -p build && cd build
+mkdir -p build && pushd "$BUILD_DIR"
 export PYTHON_DIR=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix)
 rm -f .qmake.cache
 
-pushd "$BUILD_DIR"
 qmake -Wall PYTHON_VERSION=3.$TRIK_PYTHON3_VERSION_MINOR PYTHON_PATH=/usr CONFIG+=$CONFIG $QMAKE_EXTRA "$ROOT_DIR/$PROJECT.pro"
 make -j $(nproc) qmake_all 2>&1 | tee -a build.log
 ccache -s
