e7a74f09c77fdadd32963fa4cc6dc373e467e805
MinyazevR <eeuriset8@gmail.com>
2025-09-09 23:12:30 +0300

    Fix Vera issues

--- qrtranslations/ru/qrgui_mainWindow_ru.ts
+++ qrtranslations/ru/qrgui_mainWindow_ru.ts
@@ -735,7 +735,7 @@
         <translation type="vanished">О QReal</translation>
     </message>
     <message>
-        <location filename="../../qrgui/mainWindow/mainWindow.cpp" line="+978"/>
+        <location filename="../../qrgui/mainWindow/mainWindow.cpp" line="+982"/>
         <location line="+11"/>
         <source>Error</source>
         <translation>Ошибка</translation>
@@ -788,7 +788,7 @@
         <translation>Создать диаграмму</translation>
     </message>
     <message>
-        <location line="-1153"/>
+        <location line="-1157"/>
         <source>Restore default settings</source>
         <translation>Восстановить настройки по-умолчанию</translation>
     </message>
@@ -807,7 +807,7 @@ WARNING: The settings will be restored after application restart</source>
         <translation>Не удалось сохранить файл, попробуйте сохранить его в другое место</translation>
     </message>
     <message>
-        <location line="+689"/>
+        <location line="+693"/>
         <source>Undo</source>
         <translation>Отменить</translation>
     </message>
