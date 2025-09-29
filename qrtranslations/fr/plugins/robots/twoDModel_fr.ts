27fb2f1c3eba96d9753451ac4c97ce27a5627452
MinyazevR <eeuriset8@gmail.com>
2025-09-29 14:28:51 +0300

    Correct loading of an empty xml with restrictions

--- qrtranslations/fr/plugins/robots/twoDModel_fr.ts
+++ qrtranslations/fr/plugins/robots/twoDModel_fr.ts
@@ -311,7 +311,7 @@
 <context>
     <name>twoDModel::constraints::ConstraintsChecker</name>
     <message>
-        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/constraints/constraintsChecker.cpp" line="+119"/>
+        <location filename="../../../../plugins/robots/common/twoDModel/src/engine/constraints/constraintsChecker.cpp" line="+118"/>
         <source>Error while parsing constraints: %1</source>
         <translation>Une erreur lors de l&apos;analyse des contraints est survenue : %1</translation>
     </message>
