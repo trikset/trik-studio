657d59b1407d28b32c3e6b0228a5166dad0fdbb3
MinyazevR <eeuriset8@gmail.com>
2025-11-24 20:10:01 +0300

    Add the lmsasm license to the installer

--- installer/packages/trik-studio/ru.qreal.root.ev3/meta/prebuild-common.sh
+++ installer/packages/trik-studio/ru.qreal.root.ev3/meta/prebuild-common.sh
@@ -7,4 +7,5 @@ cd "$(dirname "$0")"
 mkdir -p "$PWD"/../data/plugins/editors
 mkdir -p "$PWD"/../data/plugins/tools/kitPlugins
 mkdir -p "$PWD"/../data/examples
+rsync -a "$INSTALLER_ROOT"/licenses/Lmsasm-license.txt                      "$PWD"
 #rsync -a  "$BIN_DIR"/examples/ev3                                             "$PWD"/../data/examples
