f93f576059392c420dd65d7b8f4e1484221a9eed
MinyazevR <eeuriset8@gmail.com>
2026-02-09 01:42:41 +0300

    Improve code

--- sanitizers/sanitizers.cpp
+++ sanitizers/sanitizers.cpp
@@ -21,24 +21,21 @@
 
 std::atomic<char*> globalSanitizerLogPath{nullptr};
 
-void initSanitizerPath(const char* path) {
-	if (auto* const newPath = strdup(path)) {
-		if (auto* const oldPath = globalSanitizerLogPath.exchange(newPath)) {
-			free(oldPath);
+extern "C" {
+	void initSanitizerPath(const char* path) {
+		if (auto* const newPath = strdup(path)) {
+			if (auto* const oldPath = globalSanitizerLogPath.exchange(newPath)) {
+				free(oldPath);
+			}
 		}
 	}
-}
 
-extern "C" {
-	__attribute__((used, visibility("default")))
 	// NOLINTNEXTLINE(bugprone-reserved-identifier)
 	const char* __asan_default_options() { return "detect_leaks=1"; }
 
-	__attribute__((used, visibility("default")))
 	// NOLINTNEXTLINE(bugprone-reserved-identifier)
 	const char* __ubsan_default_options() { return "print_stacktrace=1"; }
 
-	__attribute__((used, visibility("default")))
 	// NOLINTNEXTLINE(bugprone-reserved-identifier)
 	void __sanitizer_on_print(const char* str) {
 		static FILE* file = nullptr;
